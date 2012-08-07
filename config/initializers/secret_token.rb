285eec1054df46e376b65fa7fde0b6e357ac04e8
Liu Yang <zation1@gmail.com>
2012-08-07 23:05:57 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestChao::Application.config.secret_token = '059573e318a5b932bcd4bc51a66503194864a58e40ee45811a9509ab10939da9209cbebed6b400eefb4961d1e1fc79d4b4263911416646aa2e47027556a91e4f'
