666d1fe8c518463387613be31a60eac0b4db5d3a
Ying <ying@stillnaughty.com>
2012-09-08 17:08:59 +0800

    Avoid global functions in directives by defining the controller functions within  & distinguish between private & public controller functions using 'var'

--- app/assets/controllers/magazine.controller.js
+++ app/assets/controllers/magazine.controller.js
@@ -2,6 +2,10 @@ var Magazine = {
   view: {
     controller: function($scope, $location, $routeParams) {
       $scope.id = $routeParams.id
+      // $scope.data = $resource('app/models/:model', {model:'default.json'}).get()
+      // $scope.$watch('data', function() { 
+      //   scope.data.$save();
+      // });
     }
   }
 }
