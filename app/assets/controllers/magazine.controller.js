2cb9c59c80c1f7db162eb04a0ed40cb58b675d51
Ying <ying@stillnaughty.com>
2012-09-09 16:50:29 +0800

    Unfinished Angular service using  /  module with ROR articles controller created

--- app/assets/controllers/magazine.controller.js
+++ app/assets/controllers/magazine.controller.js
@@ -1,7 +1,8 @@
 var Magazine = {
   view: {
-    controller: function($scope, $location, $routeParams) {
-      $scope.id = $routeParams.id
+    controller: function($scope, $location, $routeParams, Article) {
+//      $scope.id = $routeParams.id
+      $scope.data = Article.list({action:1506})
       // $scope.data = $resource('app/models/:model', {model:'default.json'}).get()
       // $scope.$watch('data', function() { 
       //   scope.data.$save();
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Chao5kan::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'application#template'
+  resources :articles
 
   # See how all your routes lay out with "rake routes"
 
