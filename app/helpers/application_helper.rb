36ad542ee982b78ca91cf2bfe0f242b2db8b5688
Ying <ying@stillnaughty.com>
2012-09-13 01:21:37 +0800

    Replace I18n.translate to 't' function (same as RoR) with correpsonding changes as well as using markdown on description in locale yaml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,18 @@
 module ApplicationHelper
 
+  def t ref
+    return I18n.translate ref
+  end
   def description_to_html ref
     out = I18n.translate ref
     if out.kind_of? Array
       out.map! do |item|
-        markdown_to_html(item.kind_of?(Array) ? item.join('\n') : item)
+        item.kind_of?(Array) ? item.join("  \n") : item
       end
-      return "<p>" + out.join('</p><p>') + '</p>'
+      return markdown_to_html out.join "\n\n"
     end
     return markdown_to_html out
   end
-
   def markdown_to_html string
     return BlueCloth::new(string).to_html
   end
